74768ae8fed44eeec345f12f5a67e82a13f983d5
YuliiaManivska <manivskaia.yulia@gmail.com>
2021-12-14 17:02:30 +0200

    Add migration and models

---
+++ lesson5/db/schema.rb
@@ -0,0 +1,25 @@
+ActiveRecord::Schema.define(version: 2021_12_14_134727) do
+  enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.bigint "physician_id"
+    t.bigint "patient_id"
+    t.datetime "appointment_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+    t.index ["physician_id"], name: "index_appointments_on_physician_id"
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "physicians", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+end
