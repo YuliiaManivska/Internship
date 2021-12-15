3a5153340f7d4c4a998197df4354291ea800b4ad
Yuliia Manivska <93981767+YuliiaManivska@users.noreply.github.com>
2021-12-15 22:31:00 +0200

    Add files via upload

--- lesson5/app/models/physician.rb
+++ lesson5/app/models/physician.rb
@@ -1,4 +1,12 @@
 class Physician < ApplicationRecord
+  validates :first_name, :phone_number, :level, presence: true
+  validates :level, inclusion: { in: %w(junior middle senior),
+    message: "%{value} is not a valid level" }
+  validates :phone_number, format:{ with: /\A\+380\d{9}/,
+    message: "%{value} is not a vailid phone number" }
+    
   has_many :appointments
   has_many :patients, through: :appointments
+  has_many :diagnosis_items
+  has_many :patients, through: :diagnosis_items
 end
