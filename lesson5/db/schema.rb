09b62338b8e0b18e256721640e5ea4c8d355b7c6
Yuliia Manivska <93981767+YuliiaManivska@users.noreply.github.com>
2021-12-15 22:37:43 +0200

    Add columns and new table

--- lesson5/db/schema.rb
+++ lesson5/db/schema.rb
@@ -1,4 +1,5 @@
-ActiveRecord::Schema.define(version: 2021_12_14_134727) do
+ActiveRecord::Schema.define(version: 2021_12_15_130320) do
+
   enable_extension "plpgsql"
 
   create_table "appointments", force: :cascade do |t|
@@ -11,17 +12,28 @@ ActiveRecord::Schema.define(version: 2021_12_14_134727) do
     t.index ["physician_id"], name: "index_appointments_on_physician_id"
   end
 
-  create_table "patients", force: :cascade do |t|
-    t.string "name"
+  create_table "diagnosis_items", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "first_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "last_name"
+    t.integer "age"
+    t.string "email"
   end
 
   create_table "physicians", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
+    t.string "last_name"
+    t.string "phone_number"
+    t.string "level"
   end
-  
+
 end
