711d14f5d9ec62670bca5a87bde911e305db78a9
Yuliia Manivska <93981767+YuliiaManivska@users.noreply.github.com>
2021-12-17 15:52:04 +0200

    Fixed models

--- lesson5/app/models/patient.rb
+++ lesson5/app/models/patient.rb
@@ -1,11 +1,10 @@
 class Patient < ApplicationRecord
   validates :first_name, :last_name, :age, presence: true
-  validates :first_name, :last_name, length: { in 2..25 }
+  validates :first_name, :last_name, length: { in: 2..25 }
   validates :age, numericality: { only_integer: true,
-    message "%{value} is not a valid" }
+    message: "%{value} is not a valid", in: 6..99 }
     
   has_many :appointments
   has_many :physicians, through: :appointments
   has_many :diagnosis_items
-  has_many :physicians, through: :diagnosis_items
 end
--- lesson5/app/models/physician.rb
+++ lesson5/app/models/physician.rb
@@ -8,5 +8,4 @@ class Physician < ApplicationRecord
   has_many :appointments
   has_many :patients, through: :appointments
   has_many :diagnosis_items
-  has_many :patients, through: :diagnosis_items
 end
