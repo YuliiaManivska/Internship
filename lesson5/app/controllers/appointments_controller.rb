5ee44a8fce564f900fac92f0564130aaf8f0aa5e
YuliiaManivska <manivskaia.yulia@gmail.com>
2021-12-27 17:44:59 +0200

    adds header, creates new controllers

---
+++ lesson5/app/controllers/appointments_controller.rb
@@ -0,0 +1,45 @@
+class AppointmentsController < ApplicationController
+  def index
+    @appointment = Appointment.all
+  end
+
+  def show
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    if @appointment.save
+      redirect_to @appointment
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+    if @appointment.update(appointment_params)
+      redirect_to @appointment
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @appointment = Appointment.find(params[:id])
+    @appointment.destroy
+    redirect_to appointments_path
+  end
+
+  private def appointment_params
+    params.require(:appointment).permit(:first_name, :last_name, :email, :age)
+  end
+end
