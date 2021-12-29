ecb44e10f4563b88e3e82fc52ac91f6033259f3a
YuliiaManivska <manivskaia.yulia@gmail.com>
2021-12-29 17:02:34 +0200

    Adds gem 'simple_form'

--- lesson5/app/models/patient.rb
+++ lesson5/app/models/patient.rb
@@ -1,6 +1,8 @@
 class Patient < ApplicationRecord
   validates :first_name, :last_name, :age, presence: true
   validates :first_name, :last_name, length: { in: 2..25 }
+  validates :email, format:{ with: /@/,
+  message: "%{value} is not a vailid email" }
   validates :age, numericality: { only_integer: true,
     message: "%{value} is not a valid", in: 6..99 }
     
