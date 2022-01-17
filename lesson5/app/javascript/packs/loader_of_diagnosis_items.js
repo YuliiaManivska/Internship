44b40d8454fc7fe94fca1714964eb37943fb1914
YuliiaManivska <manivskaia.yulia@gmail.com>
2022-01-17 18:49:20 +0200

    Fix diagnosis_items_loader

--- lesson5/app/javascript/packs/loader_of_diagnosis_items.js
+++ lesson5/app/javascript/packs/loader_of_diagnosis_items.js
@@ -1,17 +1,17 @@
 function setupDiagnosisItems(){
-$(".show").click(function(){
-  let patientId = $(this).attr('id');
-  
-  $.ajax({
-    url: `/diagnosis_items.json?patient_id=${$(this).attr('id')}`,
-    dataType: "json",
+  $(".show").click(function(){
+    let patientId = $(this).attr('id');
 
-    success: function (data, textStatus) {
-      const res = data.map((item) => item.content);
-      $(`#show-items${patientId}`).text(res);
-    }
-  })
-});
+    $.ajax({
+      url: `/diagnosis_items.json?patient_id=${$(this).attr('id')}`,
+      dataType: "json",
+
+      success: function (data, textStatus) {
+        const res = data.map((item) => item.content);
+        $(`#show-items${patientId}`).text(res);
+      }
+    })
+  });
 }
 $(document).on('turbolinks:load', function() {
   setupDiagnosisItems();
