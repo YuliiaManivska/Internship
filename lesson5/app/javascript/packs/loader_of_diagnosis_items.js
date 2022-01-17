98171a444eb09d0475563b689b0188416b9a6487
YuliiaManivska <manivskaia.yulia@gmail.com>
2022-01-17 18:42:07 +0200

    Fix diagnosis_items_loader

--- lesson5/app/javascript/packs/loader_of_diagnosis_items.js
+++ lesson5/app/javascript/packs/loader_of_diagnosis_items.js
@@ -1,12 +1,13 @@
 function setupDiagnosisItems(){
 $(".show").click(function(){
+  let patientId = $(this).attr('id');
+  
   $.ajax({
     url: `/diagnosis_items.json?patient_id=${$(this).attr('id')}`,
     dataType: "json",
 
     success: function (data, textStatus) {
       const res = data.map((item) => item.content);
-      let patientId = data.map((item) => item.patient_id) ;
       $(`#show-items${patientId}`).text(res);
     }
   })
